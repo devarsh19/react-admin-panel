f0235d928ebc4a66221bf861c98dbb5e6fe8d932
devarsh19 <devarsh96patel@gmail.com>
2020-08-26 00:29:19 -0600

    Updates

---
+++ precache-manifest.42b0a6f96bbee63e59626c928bac9b78.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa743782c8a12e5e40d8db5d4ddd6f3",
+    "url": "/react-admin-panel/index.html"
+  },
+  {
+    "revision": "dae4fab51ec32a9081b0",
+    "url": "/react-admin-panel/static/css/2.cc4bd3a9.chunk.css"
+  },
+  {
+    "revision": "bfa5aa9def235da33ca4",
+    "url": "/react-admin-panel/static/css/main.f7fca948.chunk.css"
+  },
+  {
+    "revision": "dae4fab51ec32a9081b0",
+    "url": "/react-admin-panel/static/js/2.11fbaa67.chunk.js"
+  },
+  {
+    "revision": "bfa5aa9def235da33ca4",
+    "url": "/react-admin-panel/static/js/main.0c3ba727.chunk.js"
+  },
+  {
+    "revision": "ea857d08ee20a12d2735",
+    "url": "/react-admin-panel/static/js/runtime~main.2307e816.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/react-admin-panel/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/react-admin-panel/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/react-admin-panel/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/react-admin-panel/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/react-admin-panel/static/media/face-3.ce7a6b79.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-admin-panel/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-admin-panel/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-admin-panel/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-admin-panel/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-admin-panel/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "9b864b3680f372ff9ae02649fbc44838",
+    "url": "/react-admin-panel/static/media/reactlogo.9b864b36.png"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/react-admin-panel/static/media/sidebar-1.6be21e8a.jpg"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/react-admin-panel/static/media/sidebar-2.7a4ce7cc.jpg"
+  },
+  {
+    "revision": "34535b958ad0f0183770a8731b31b4ca",
+    "url": "/react-admin-panel/static/media/sidebar-3.34535b95.jpg"
+  },
+  {
+    "revision": "7bc259fd5cef15e95376c822c7e23081",
+    "url": "/react-admin-panel/static/media/sidebar-4.7bc259fd.jpg"
+  }
+]);
